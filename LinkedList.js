495b6a7eebe1a64addd0bca4973dc215f8a521bf
dymjin <dymjin@gmail.com>
2025-09-30 11:38:38 +0200

    Add all methods + head

---
+++ LinkedList.js
@@ -0,0 +1,16 @@
+import { Node } from "./Node.js";
+export class LinkedList {
+  #head = null;
+  append(value) {}
+  prepend(value) {}
+  size() {}
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(value) {}
+  toString() {}
+  insertAt(value, index) {}
+  removeAt(index) {}
+}
