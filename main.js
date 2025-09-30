3683ecac8c25d1d4aa253a535fd9549a07487483
dymjin <dymjin@gmail.com>
2025-09-30 15:48:39 +0200

    Add append test

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import { LinkedList } from "./LinkedList.js";
 const list = new LinkedList();
 
+list.append("bear");
+list.append("squid");
+list.append("bee");
 list.prepend("pear");
-list.prepend("mango");
-list.prepend("apple");
+// list.prepend("mango");
+// list.prepend("apple");
 
 // list.pop();
 // console.log(list.head());
 // console.log(list.tail());
-// console.log(list.size());
+console.log(list.size());
 // console.log(list.at(0));
-console.log(list.toString())
+console.log(list.toString());
