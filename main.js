3aab46c88bc9d1948b5dce8e7424b473dd1c71c2
dymjin <dymjin@gmail.com>
2025-09-30 15:55:14 +0200

    Add find() test

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ list.prepend("pear");
 // console.log(list.head());
 // console.log(list.tail());
 // console.log(list.size());
-console.log(list.contains("squdid"));
+// console.log(list.contains("squdid"));
+console.log(list.find("bee"));
 // console.log(list.at(0));
 console.log(list.toString());
